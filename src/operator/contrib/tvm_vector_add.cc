c0d31686d6afa4f0875713a7575fe107c9800b39
Junru Shao <junrushao1994@gmail.com>
2019-05-24 09:08:21 -0700

    minor tweaks

--- src/operator/contrib/tvm_vector_add.cc
+++ src/operator/contrib/tvm_vector_add.cc
@@ -12,24 +12,24 @@ class TVMCUDAFunctor {
  public:
   // constructor
   explicit TVMCUDAFunctor(const std::string& name) {
-    static const PackedFunc* f_so = Registry::Get("module.loadfile_so");
-    static const PackedFunc* f_ptx = Registry::Get("module.loadfile_ptx");
-    Module m_so = (*f_so)(name + ".so");
-    Module m_ptx = (*f_ptx)(name + ".ptx");
+    static const PackedFunc* f_load = Registry::Get("module._LoadFromFile");
+    Module m_so = (*f_load)(name + ".so", "");
+    Module m_ptx = (*f_load)(name + ".ptx", "");
     m_so.Import(m_ptx);
     func = m_so.GetFunction("__tvm_main__", false);
   }
 
-  void Prepare(const std::vector<mxnet::NDArray> &args) {
+  TVMArgs Prepare(const std::vector<mxnet::NDArray> &args) {
     type_codes.resize(args.size());
     values.resize(args.size());
     for (int i = 0; i < static_cast<int>(args.size()); ++i) {
-      type_codes[i] = mxnet::kTVMNDArrayTypeCode;
+      type_codes[i] = kArrayHandle;
       values[i].v_handle = const_cast<DLTensor*>(&(args[i].data().dltensor()));
     }
+    return TVMArgs(&values[0], &type_codes[0], args.size());
   }
- public:
 
+ public:
   std::vector<TVMValue> values;
   std::vector<int> type_codes;
   PackedFunc func;
@@ -60,21 +60,23 @@ static void TVMVectorAddStorageComputeExGPU(const nnvm::NodeAttrs& attrs,
                                             const std::vector<NDArray>& inputs,
                                             const std::vector<OpReqType>& req,
                                             const std::vector<NDArray>& outputs) {
-  using namespace tvm::runtime;
-  static const PackedFunc* f_set_stream = Registry::Get("_TVMSetStream");
-  thread_local TVMCUDAFunctor func("/home/ubuntu/Projects/tvm-compiler/build/myadd");
+  using tvm::runtime::Registry;
+  using tvm::runtime::PackedFunc;
+  using tvm::runtime::TVMArgs;
+  using tvm::runtime::TVMRetValue;
+  using tvm::runtime::TVMCUDAFunctor;
+
+  static thread_local TVMCUDAFunctor func("/home/ubuntu/Projects/tvm-compiler/build/myadd");
   CHECK_EQ(inputs.size(), 2U);
   CHECK_EQ(outputs.size(), 1U);
-  func.Prepare({inputs[0], inputs[1], outputs[0]});
-
+  TVMArgs args = func.Prepare({inputs[0], inputs[1], outputs[0]});
   TVMRetValue rv;
-  TVMArgs args(&func.values[0], &func.type_codes[0], 3);
-  void* stream = ctx.run_ctx.stream;
+  void* stream = static_cast<void*>(ctx.run_ctx.get_stream<gpu>()->stream_);
   int dev_type = kDLGPU;
   int dev_id = ctx.run_ctx.ctx.dev_id;
-  (*f_set_stream)(dev_type, dev_id, stream);
+  TVMSetStream(dev_type, dev_id, stream);
   func.func.CallPacked(args, &rv);
-  (*f_set_stream)(dev_type, dev_id, nullptr);
+  TVMSetStream(dev_type, dev_id, nullptr);
 }
 }  // namespace op
 }  // namespace mxnet
--- test_tvm_bridge2.py
+++ test_tvm_bridge2.py
@@ -2,4 +2,9 @@ import mxnet as mx
 
 a = mx.nd.array([1, 2, 3, 4, 5], ctx=mx.gpu(0))
 b = mx.nd.array([5, 4, 3, 2, 1], ctx=mx.gpu(0))
-print(mx.nd.tvm_vector_add(a, b))
+c = mx.nd.tvm_vector_add(a, b)
+print(a.asnumpy())
+print(b.asnumpy())
+print(c.asnumpy())
+c.wait_to_read()
+print("Program shutdown!!!")
